e3d5126286bcbbe5820c657c7d8b6f3b07f9024c
ogaillard63 <ogaillard63@gmail.com>
2017-02-09 00:07:44 +0100

    Corrections bugs

--- thermostat.ino
+++ thermostat.ino
@@ -3,238 +3,182 @@
 #include <OneWire.h>
 #include <RCSwitch.h>
 #include <Button.h>
-#include "data.h"
+#include <DallasTemperature.h>
+#include <avr/pgmspace.h>
 
-#define DEBUGMODE        1
-// OLED I2C bus address
-#define OLED_address  0x3c
-#define BUTTON1_PIN     12
-#define DS18B20_PIN     2
-#define LED_PIN         13
-#define REMOTE_TX_PIN   7
+// Generated with GLCD Tools (output Microchip)
+// battery level 16x8
+uint8_t const battery[][16] PROGMEM = {
+    {0x1c, 0x7f, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x7f},
+    {0x1c, 0x7f, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x5d, 0x5d, 0x41, 0x7f},
+    {0x1c, 0x7f, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x5d, 0x5d, 0x41, 0x5d, 0x5d, 0x41, 0x7f},
+    {0x1c, 0x7f, 0x41, 0x41, 0x41, 0x41, 0x5d, 0x5d, 0x41, 0x5d, 0x5d, 0x41, 0x5d, 0x5d, 0x41, 0x7f},
+    {0x1c, 0x7f, 0x41, 0x5d, 0x5d, 0x41, 0x5d, 0x5d, 0x41, 0x5d, 0x5d, 0x41, 0x5d, 0x5d, 0x41, 0x7f}};
+
+#define DEBUGMODE 0
+#define OLED_address 0x3c // OLED I2C bus address
+#define BUTTON1_PIN 12
+#define DS18B20_PIN 2
+#define TEMPERATURE_PRECISION 10 // Lower resolution
+#define REMOTE_TX_PIN 7
+#define SWITCH_ON_CODE 1381719
+#define SWITCH_OFF_CODE 1381716
 
 // Déclaration de l'objet ds
-OneWire ds(DS18B20_PIN); // on pin DS18B20_PIN (a 4.7K resistor is necessary)
-
+OneWire oneWire(DS18B20_PIN); // on pin DS18B20_PIN (a 4.7K resistor is necessary)
+DallasTemperature sensors(&oneWire);
 TinyOLED tinyOLED(0x3C); // SPI Address
 RCSwitch mySwitch = RCSwitch();
 
-const int DS18B20_ID=0x28;
-
 // GND -----/ button ------ pin 12
-Button button1 = Button(BUTTON1_PIN,BUTTON_PULLUP_INTERNAL);
-boolean longPush = false;
-boolean refresh = true; // refresh display
-char buf [4];
+Button button1 = Button(BUTTON1_PIN, BUTTON_PULLUP_INTERNAL);
+char buf[4];
 
 float temperature;
 float last_temperature = 0.0f;
-
-// debug
-int index = 0;
-int sampleTemp[] = { 15,16,17,18,19,20,21,22,23,24,25,24,23,22,21,20,19,18,17,16};      
 
 boolean heatingOn = false;
 boolean setOn = false;
 boolean isHold = false;
 
-float seuil = 18;
+float threshold = 18;
 float hysteresis = 1;
-
 float tempMin = 14;
 float tempMax = 25;
+DeviceAddress tempDeviceAddress; // sensor device address
 
 void setup() {
   if (DEBUGMODE) {
     Serial.begin(9600);
-    Serial.println("Initialisation du programme");
+    Serial.println("RF Thermostat init ...");
   }
   // Init button
   button1.releaseHandler(handleButtonReleaseEvents);
-  button1.holdHandler(handleButtonHoldEvents,2000);
- 
+  button1.holdHandler(handleButtonHoldEvents, 2000);
+
   mySwitch.enableTransmit(REMOTE_TX_PIN);
-  mySwitch.setPulseLength(450); //set pulse length
-  mySwitch.setProtocol(1, 450); // set protocol
+  mySwitch.setPulseLength(450);   //set pulse length
+  mySwitch.setProtocol(1, 450);   // set protocol
   mySwitch.setRepeatTransmit(10); // transmission repetitions
-  
+
+  // Activate sensors
+  sensors.begin();
+  sensors.getAddress(tempDeviceAddress, 0);
+  sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
+
   // Initialize I2C and OLED Display
   Wire.begin();
-  tinyOLED.init();  //init OLED display
-  initScreen();
+  tinyOLED.init(); //init OLED display
+  displayHeader();
 }
 
-void loop() {  
- button1.isPressed();
-
- if (!setOn) {
-   temperature = getTemperatureDS18b20(); // On lance la fonction d'acquisition de T° 
-   if (temperature < (seuil -(hysteresis/2))) turnOn();
-   if (temperature > (seuil + (hysteresis/2))) turnOff();
-   displayInfos();
- }
- 
- if (DEBUGMODE) {
-    Serial.print("Temperature : "); 
-    Serial.println(temperature);
+void loop() {
+  button1.isPressed();
+  if (!setOn) {
+    sensors.requestTemperatures();
+    temperature = sensors.getTempCByIndex(0);
+    if (last_temperature != temperature) {
+      last_temperature = temperature;
+      if (temperature < (threshold - (hysteresis / 2))) turnOn();
+      if (temperature > (threshold + (hysteresis / 2))) turnOff();
+      displayInfos();
+      debug("Temperature : ", temperature);
     }
   }
-
-void showTemperature(float t) {
-  int partent = (int) t;
-  int partdec = (t - partent)*10;
-
-  sprintf (buf, "%i,%i", partent, partdec);
-  tinyOLED.drawBigNums(0,2, buf);
+  
 }
 
-void showLocation() {
-  tinyOLED.drawStringXY(0,2, "45.6541358"); 
-  tinyOLED.drawStringXY(0,4, "03.2458689"); 
-}
 
+// Turn ON Remote
 void turnOn() {
-    mySwitch.send(1381719, 24);
-    delay(500);
-    digitalWrite(LED_PIN, HIGH);
-    heatingOn = true;
-    if (DEBUGMODE) Serial.println("Heating ON !");
+  mySwitch.send(SWITCH_ON_CODE, 24);
+  delay(500);
+  heatingOn = true;
+  if (DEBUGMODE)
+    Serial.println("Heating ON !");
 }
-
+// Turn OFF Remote
 void turnOff() {
-    digitalWrite(LED_PIN, LOW);
-    delay(500);
-    mySwitch.send(1381716, 24);
-    heatingOn = false;    
-    if (DEBUGMODE) Serial.println("Heating OFF !");
+  delay(500);
+  mySwitch.send(SWITCH_OFF_CODE, 24);
+  heatingOn = false;
+  if (DEBUGMODE)
+    Serial.println("Heating OFF !");
 }
-
-/* -------- Gestion de l'affichage -------- */
+// Display screen infos
 void displayInfos() {
-  initScreen();   // clears the screen and buffer
-  showTemperature(temperature);
+  displayHeader(); 
+  showTempData(temperature, 1);
   if (setOn) {
-    tinyOLED.drawStringXY(1, 7, "SETTING");
+    tinyOLED.drawStringXY(7, 7, "SET");
   }
   else {
-    if (heatingOn) tinyOLED.drawStringXY(1, 7, "ON           ");
-    else tinyOLED.drawStringXY(1, 7, "OFF          ");
+    if (heatingOn)
+      tinyOLED.drawStringXY(1, 7, "ON       ");
+    else
+      tinyOLED.drawStringXY(1, 7, "OFF      ");
   }
-
-  sprintf (buf, "%i", (int)(seuil*100));
-  tinyOLED.drawStringXY(10, 7, buf);
+  showTempData(threshold, 2);
 }
-
-void initScreen() {
-  tinyOLED.drawStringXY(0,0, " THERMOSTAT"); 
+// Display screen header
+void displayHeader() {
+  tinyOLED.drawStringXY(0, 0, " THERMOSTAT");
   showBatterylevel(readVcc());
 }
-
-/* ---------- Gestion du boutons ---------- */
-
-// Bouton relaché
+// Display temperature on screen
+void showTempData(float t, int pos) {
+  float dec = (t-(int)t) * 10;
+  sprintf(buf, "%d,%d", (int)t, (int)dec);
+  if (pos == 1) tinyOLED.drawBigNums(2, 2, buf);
+  if (pos == 2) tinyOLED.drawStringXY(11, 7, buf);
+  
+}
+// Button is Release
 void handleButtonReleaseEvents(Button &btn) {
   if (!isHold) {
     if (setOn) {
-        flashLed();
-        seuil+=0.5f;
-        if (seuil > tempMax) seuil = tempMin;
-        displayInfos();
-        }
+      threshold += 0.5f;
+      if (threshold > tempMax)
+        threshold = tempMin;
+      displayInfos();
     }
-    isHold = false;
   }
-  
-// Appui prolongé sur le bouton
-void handleButtonHoldEvents(Button &btn)  {
-  isHold = true;
-  if (setOn == true) {
-    setOn = false;
-    digitalWrite(LED_PIN, LOW);
-    displayInfos();
-  }
-  
-  else {
-    setOn = true;
-    digitalWrite(LED_PIN, HIGH);
-    displayInfos();
-  }
- }
- 
-/* --------------------------------------------------------- */
-void flashLed(){
-    digitalWrite(LED_PIN, LOW);   // turn the LED on (HIGH is the voltage level)
-    delay(100);                   // wait for a second
-    digitalWrite(LED_PIN, HIGH);  // turn the LED off by making the voltage LOW
-    delay(100);
-} 
-/* --------------- Acquisition de la température ----------------------------------- */
-float _getTemperatureDS18b20(){
-  if (index++ > 18) index = 0;
-  delay(850); 
-  return (float)sampleTemp[index];
+  isHold = false;
 }
-
-float getTemperatureDS18b20(){
-  byte i;
-  byte data[12];
-  byte addr[8];
-  float temp =0.0;
-  
-  //Il n'y a qu'un seul capteur, donc on charge l'unique adresse.
-  ds.search(addr);
-  
-  // Cette fonction sert à surveiller si la transmission s'est bien passée
-  if (OneWire::crc8( addr, 7) != addr[7]) {
-  return false;
-  }
-  
-  // On vérifie que l'élément trouvé est bien un DS18B20
-  if (addr[0] != DS18B20_ID) {
-  return false;
-  }
-  
-  // Demander au capteur de mémoriser la température et lui laisser 850ms pour le faire (voir datasheet)
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44);
-  delay(850);
-  // Demander au capteur de nous envoyer la température mémorisé
-  ds.reset();
-  ds.select(addr);
-  ds.write(0xBE);
-  
-  // Le MOT reçu du capteur fait 9 octets, on les charge donc un par un dans le tableau data[]
-  for ( i = 0; i < 9; i++) {
-  data[i] = ds.read();
-  }
-  // Puis on converti la température (*0.0625 car la température est stockée sur 12 bits)
-  temp = ( (data[1] << 8) + data[0] )*0.0625;
-  
-  return temp;
+// Button is hold
+void handleButtonHoldEvents(Button &btn) {
+ isHold = true;
+ setOn = !setOn ;
+ displayInfos();
 }
-
-
-/* ---- */
 // Show battery level icon
 void showBatterylevel(long vcc) {
   byte level = 0;
-  if (vcc > 3600) level=4; 
-  else if (vcc > 3400) level=3; 
-  else if (vcc > 3200) level=2; 
-  else if (vcc > 3000) level=1; 
-  tinyOLED.drawBitmap(14, 0, battery[level], 16, 8); 
+  if (vcc > 3600)
+    level = 4;
+  else if (vcc > 3400)
+    level = 3;
+  else if (vcc > 3200)
+    level = 2;
+  else if (vcc > 3000)
+    level = 1;
+  tinyOLED.drawBitmap(14, 0, battery[level], 16, 8);
 }
 // Read VCC
 long readVcc() {
   long result;
   // Read 1.1V reference against AVcc
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  delay(2); // Wait for Vref to settle
+  delay(2);            // Wait for Vref to settle
   ADCSRA |= _BV(ADSC); // Convert
-  while (bit_is_set(ADCSRA,ADSC));
+  while (bit_is_set(ADCSRA, ADSC));
   result = ADCL;
-  result |= ADCH<<8;
+  result |= ADCH << 8;
   result = 1126400L / result; // Back-calculate AVcc in mV
   return result;
 }
+// Display debug infos
+void debug(String str, float value) {
+ if (DEBUGMODE) Serial.print(str);
+ if (DEBUGMODE) Serial.println(value);
+} 
